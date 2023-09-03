4acfc3ad41a8b9fc139c5114bc0348b700ee6e2e
Sampath Sanjeeva Billava <Sampath.Billava@novigosolutions.com>
2023-09-03 10:53:37 +0530

    Add project files.

---
+++ WindowsAuth/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WindowsAuth.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    //[AllowAnonymous]
+    [Authorize]
+    [EnableCors("CorsPolicy")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string MethodOne()
+        {
+            try
+            {
+                return "MethodOne is called successfully";
+            }
+            catch (Exception ex)
+            {
+                return $"Error: {ex.Message}. ->>> {ex.InnerException}";
+            }
+        }
+    }
+}
---
+++ WindowsAuth/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Authentication.Negotiate;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+   .AddNegotiate();
+
+builder.Services.AddAuthorization(options =>
+{
+    // By default, all incoming requests will be authorized according to the default policy.
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+builder.Services.AddRazorPages();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+        builder =>
+        {
+            builder
+                //.WithOrigins("http://example.com", "http://example2.com")
+                .AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseCors();
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
